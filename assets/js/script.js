1765c72b3398c134f7da01f094c884764907f973
Daniel Elde-Johansson <biz45dy2@students.codeinstitute.net>
2024-11-24 11:24:17 +0000

    Added 15 questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,192 @@
 //  quiz questions
 
 const questions = [
+
     {
-        question: "Where does the human race start?"
-    }
-]
+        question: "Where does the human race start?",
+        answers: [
+          {
+            text: "Goldshire.",
+            correct: false,
+          },
+          {
+            text: "Orgrimmar.",
+            correct: false,
+          },
+          { text: "Thunderbluff.", 
+            correct: false },
+          {
+            text: "Northshire Valley.",
+            correct: true,
+          },
+        ],
+    },
+    {
+        question: "Who was the warchief in classic?",
+        answers: [
+          { text: "Garrosh Hellscream.", 
+            correct: false },
+          { text: "Jaina Proudmore.", 
+            correct: false },
+          {
+            text: "Thrall.",
+            correct: true,
+          },
+          { text: "Brann Bronzebeard.", 
+            correct: false },
+        ],
+    },
+    {
+        question: "What is the best currency?",
+        answers: [
+          { text: "Silver.", 
+            correct: false },
+          { text: "Bronze.", 
+            correct: false },
+          { text: "Diamond.", 
+            correct: false },
+          { text: "Gold.",
+             correct: true },
+        ],
+    },
+    {
+        question: "What is the name of the Alliance King?",
+        answers: [
+          { text: "Anduin Wrynn", 
+            correct: false },
+          { text: "Varian Wrynn",
+             correct: true },
+          { text: "Sylvanas Windrunner",
+             correct: false },
+          { text: "Khadgar",
+             correct: false },
+        ],
+    },
+    {
+        question: "How many Classes is there in The War Within?",
+        answers: [
+          { text: "13.",
+             correct: true },
+          { text: "11.",
+             correct: false },
+          { text: "14.",
+             correct: false },
+          { text: "12.",
+             correct: false },
+        ],
+      },
+      {
+        question: "How many playable Races are there?",
+        answers: [
+          { text: "25.",
+             correct: false },
+          { text: "26.",
+             correct: true },
+          { text: "27.",
+             correct: false },
+          { text: "24.",
+             correct: false },
+        ],
+      },
+      {
+        question: "Who is the Litch king ?",
+        answers: [
+          { text: "Thrall",
+            correct: false },
+          { text: "Varian Wrynn .",
+            correct: false },
+          { text: "Arthas Menethil.",
+            correct: true,},
+          { text: "Jaina Proudmore.",
+            correct: false },
+        ],
+      },
+      {
+        question: "How many proffesions are there?",
+        answers: [
+          { text: "13.",
+             correct: true },
+          { text: "12.",
+             correct: false },
+          { text: "14.",
+             correct: false },
+          { text: "11.",
+             correct: false },
+        ],
+      },
+      {
+        question: "Witch year did World of Warcraft get realeased ?",
+        answers: [
+          { text: "2003.",
+             correct: false },
+          { text: "2004.",
+            correct: true,},
+          { text: "2002.",
+             correct: false },
+          { text: "2005.",
+             correct: false },
+        ],
+      },
+      {
+        question: "What is the name of the race that starts neutral?",
+        answers: [
+          { text: "Human .",
+             correct: false },
+          { text: "Orc.", 
+            correct: false },
+          { text: "Pandarian.",
+             correct: true },
+          { text: "Tauren.",
+             correct: false },
+        ],
+      },
+      {
+        question: "Which raid instance is located in Stranglethorn Vale in World of Warcraft?",
+        answers: [
+          { text: "Onyxia's Lair.",
+             correct: false },
+          { text: "Zul'Farrak.",
+             correct: false },
+          { text: "Zul'Aman.",
+             correct: false },
+          { text: "Zul'Gurub.",
+            correct: true,},
+        ],
+      },
+      {
+        question: "What is the name of the Alliance capital?",
+        answers: [
+          { text: "Stormwind.", correct: true,},
+          { text: "Orgrimmar.", correct: false,},
+          { text: "Dalaran.", correct: false },
+          { text: "Thunder Bluff.", correct: false },
+        ],
+      },
+      {
+        question: "What is the name of Horde capital?",
+        answers: [
+            { text: "Orgrimmar.", correct: true,},
+            { text: "Stormwind.", correct: false,},
+            { text: "Dalaran.", correct: false },
+            { text: "Thunder Bluff.", correct: false },
+        ],
+      },
+      {
+        question: "What is the maximum level a character can reach in World of Warcraft The War Whitin?",
+        answers: [
+          { text: "90.", correct: false },
+          { text: "70.", correct: false },
+          { text: "60.", correct: false },
+          { text: "80.", correct: true,},
+        ],
+      },
+      {
+        question: "What is the name of the legendary sword wielded by the character Arthas Menethil?",
+        answers: [
+          { text: "lightmourne.", correct: false },
+          { text: "sunmourne.", correct: false },
+          { text: "Frostmourne.", correct: true,},
+          { text: "Shadowmourne.", correct: false },
+        ],
+      },
+];
