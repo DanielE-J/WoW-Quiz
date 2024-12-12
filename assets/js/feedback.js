e3c459e7eb5d84970cc3763b9bed6629bfe5196d
Daniel Elde-Johansson <biz45dy2@students.codeinstitute.net>
2024-12-12 08:24:37 +0000

    trying to fix feedback.js

--- assets/js/feedback.js
+++ assets/js/feedback.js
@@ -1,22 +1,8 @@
 /*jshint esversion: 6 */
 
-/**
- * Removes the contact forme and displays a thank you message on the contact page after form submision
- */
-function toggleThankYouMessage() {
-    let html = `
-            <p class="text-center fs-2">Thank you for leaving a message.</p>
-            <div class="image-container text-center col-5 col-sm-4 col-lg-3">
-            </div>
-            <div class="text-center my-2">
-                <a href="index.html" class="return-btn btn btn-primary">Return to game</a>
-            </div>`;
-    document.getElementById('container').innerHTML = html;
-}
-
 document.addEventListener('DOMContentLoaded', () => {
     const form = document.getElementById('contact-form');
-    const submitButton = document.getElementById('submit');
+    
 
     form.addEventListener('submit', function (event) {
         event.preventDefault(); // Prevent the form from submitting
@@ -43,4 +29,3 @@ document.addEventListener('DOMContentLoaded', () => {
         form.reset(); // Reset the form
     });
 });
-
